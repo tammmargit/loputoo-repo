9a618994a057941886fe73bdd3c90cee93297074
margittammeorg <margit.tammeorg@gmail.com>
2025-02-06 12:00:00 +0200

    Initial test files

---
+++ seltskonnamangTests/seltskonnamangTests.swift
@@ -0,0 +1,16 @@
+//
+//  seltskonnamangTests.swift
+//  seltskonnamangTests
+//
+//  Created by Margit Tammeorg on 06.02.2025.
+//
+
+import Testing
+
+struct seltskonnamangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ seltskonnamangUITests/seltskonnamangUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  seltskonnamangUITestsLaunchTests.swift
+//  seltskonnamangUITests
+//
+//  Created by Margit Tammeorg on 06.02.2025.
+//
+
+import XCTest
+
+final class seltskonnamangUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
